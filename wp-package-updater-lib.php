344c5385dacb59f749c1ff237bf09ed0f7e7b4b6
Olivier van Helden <olivier@van-helden.net>
2023-06-07 08:52:46 -0400

    added wp-package-updater and plugin-update-checker libraries

---
+++ wp-package-updater-lib.php
@@ -0,0 +1,25 @@
+<?php
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+// Enable plugin updates only if $wppul_server variable is set
+if ( isset( $wppul_server ) ) {
+
+    // Include the library file
+    require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+
+    // Instantiate WP_Package_Updater class
+    new WP_Package_Updater(
+        $wppul_server,
+        wp_normalize_path( plugin_dir_path( __FILE__ ) . 'project-donations-wc.php' ),
+        wp_normalize_path( plugin_dir_path( __FILE__ ) ),
+        isset($wppul_licence_required) ? $wppul_licence_required : false,
+    );
+
+    // Unset the variable after it has been used
+    unset( $wppul_server );
+    unset( $wppul_licence_required );
+}
