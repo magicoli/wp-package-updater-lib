e95ea71e8a10b859856a1e7ec43029b17047cb31
Olivier van Helden <olivier@van-helden.net>
2023-06-07 20:11:30 -0400

    v0.1.7 fix wrong plugin file passed to WP_Package_Updater class

--- build/package-updater.php
+++ build/package-updater.php
@@ -5,8 +5,6 @@
  * This is a set of all needed libraries to include in a plugin, to get
  * automatic  updates from Anexandre Froger's [WP Plugin Update
  * Server](https://github.com/froger-me/wp-plugin-update-server).
- *
- * Version: 0.1.6
  */
 
 namespace MagicOli\WpPackageUpdaterLib;
@@ -14,23 +12,22 @@ namespace MagicOli\WpPackageUpdaterLib;
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
-error_log('Been here ' . __FILE__);
 
 // Enable plugin updates only if $wppul_server variable is set
-if ( isset( $wppul_server ) ) {
+$trace = debug_backtrace();
+if ( isset($wppul_server) && isset($trace[0]) ) {
+	$plugin_file = empty($plugin_file) ? $trace[0]['file'] : $plugin_file;
 
-    // Include the library file
-    require_once plugin_dir_path( __FILE__ ) . 'wp-package-updater/class-wp-package-updater.php';
+	// Instantiate WP_Package_Updater class
+	require_once plugin_dir_path( __FILE__ ) . 'wp-package-updater/class-wp-package-updater.php';
+	new WP_Package_Updater(
+		$wppul_server,
+		wp_normalize_path( $plugin_file ),
+		wp_normalize_path( plugin_dir_path( $plugin_file ) ),
+		isset($wppul_licence_required) ? $wppul_licence_required : false
+	);
 
-    // Instantiate WP_Package_Updater class
-    new WP_Package_Updater(
-        $wppul_server,
-        wp_normalize_path( plugin_dir_path( __FILE__ ) . 'project-donations-wc.php' ),
-        wp_normalize_path( plugin_dir_path( __FILE__ ) ),
-        isset($wppul_licence_required) ? $wppul_licence_required : false
-    );
-
-    // Unset the variable after it has been used
-    unset( $wppul_server );
-    unset( $wppul_licence_required );
+	// Unset the variable after it has been used
+	unset( $wppul_server );
+	unset( $wppul_licence_required );
 }
