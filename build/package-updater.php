e822132741c08d054fb97f249b1143c220f30e6f
Olivier van Helden <olivier@van-helden.net>
2023-06-09 08:42:28 -0400

    v0.1.10 fix conflict if $plugin_file is set by another plugin

--- build/package-updater.php
+++ build/package-updater.php
@@ -14,18 +14,19 @@ if ( ! defined( 'WPINC' ) ) {
 // Enable plugin updates only if $wppul_server variable is set
 $trace = debug_backtrace();
 if ( isset($wppul_server) && isset($trace[0]) ) {
-	$plugin_file = empty($plugin_file) ? $trace[0]['file'] : $plugin_file;
+	$wppul_plugin_file = empty($wppul_plugin_file) ? $trace[0]['file'] : $wppul_plugin_file;
 
 	// Instantiate WP_Package_Updater class
 	require_once plugin_dir_path( __FILE__ ) . 'wp-package-updater/class-wp-package-updater.php';
 	new WP_Package_Updater(
 		$wppul_server,
-		wp_normalize_path( $plugin_file ),
-		wp_normalize_path( plugin_dir_path( $plugin_file ) ),
+		wp_normalize_path( $wppul_plugin_file ),
+		wp_normalize_path( plugin_dir_path( $wppul_plugin_file ) ),
 		isset($wppul_licence_required) ? $wppul_licence_required : false
 	);
 
 	// Unset the variable after it has been used
 	unset( $wppul_server );
 	unset( $wppul_licence_required );
+	unset( $wppul_plugin_file );
 }
