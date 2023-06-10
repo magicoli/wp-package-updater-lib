80451e8380b0675ccbbc107d637f1a6213e2f6f9
Olivier van Helden <olivier@van-helden.net>
2023-06-10 19:50:14 -0400

    v1.1.1 remove now unrelated LICENSE and README duplicates

--- build/package-updater.php
+++ build/package-updater.php
@@ -11,7 +11,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-( ! defined( 'WPPUL_VERSION' ) ) && define( 'WPPUL_VERSION', '1.1');
+( ! defined( 'WPPUL_VERSION' ) ) && define( 'WPPUL_VERSION', '1.1.1');
 
 // Enable plugin updates only if $wppul_server variable is set
 $trace = debug_backtrace();
